e886c6585561b3b318e5922b2534e97dd2c49d5e
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2026-05-05 21:17:32 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,10 +2,10 @@ class Calculator:
 
     def __init__(self):
         pass
-
+    ## Add Function
     def add(self, x, y):
         return x + y
-
+    ## Divide Function
     def divide(self, x, y):
         if y == 0:
             raise ValueError("Division by zero is not allowed.")
